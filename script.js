4a8505af0437b15ab0cf4248110a9323817906c6
Miraz9907 <meraz.rahman99@gmail.com>
2022-09-14 23:04:29 +0600

    all seconds count in parseInt

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
   
   // show result modal
   resultModal.innerHTML = "";
@@ -110,7 +110,7 @@ const start = () => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count == 0) {
+    if (count == -1) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
